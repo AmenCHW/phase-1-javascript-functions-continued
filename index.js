ad9c0dcc287e0dee684e562b18cedc62524d6a5a
Ameen Abdulsamad <amenchawshly98@gmail.com>
2023-01-27 20:39:16 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun(event = "roller-skate"){
+    return `This Saturday, I want to ${event}!`
+}
+saturdayFun()
+saturdayFun("bathe my dog")
+
+let mondayWork = function(event= "go to the office"){
+    return `This Monday, I will ${event}.`
+}
+mondayWork("go to the office")
+
+function wrapAdjective(event = "*"){
+    return function(inner = "special"){
+        return `You are ${event}${inner}${event}!`
+    }
+}
+wrapAdjective("%")("a dedicated programmer");
